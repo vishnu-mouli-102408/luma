2d77793e51bb5f4cf3918a4f26546dc723d06047
Vishnu Mouli <vishnumouli0@gmail.com>
2025-08-16 10:55:28 +0530

    added shadcn, tailwindcss, favicon setup

---
+++ client/src/components/global/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<NextThemesProvider attribute="class" enableSystem disableTransitionOnChange enableColorScheme>
+			{children}
+		</NextThemesProvider>
+	);
+}
